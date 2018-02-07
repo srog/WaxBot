acaffe3661476bf6ca1efbce589181cad407c2bf
Simon Rogers <s.rogers@waxdigital.com>
2018-02-07 15:14:23 +0000

    initial commit of files from azure

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=waxbotclient-Web-Deploy.pubxml -p:Password=LWTZwWK3AA6mNGnhjv1FqD4PjmiEWRqhhbR1cB9RGydA74uNZf7iwxip4ZHT
+
