b0c34bd1c8a935126120bf38c0fe67656997faa0
Simon Rogers <s.rogers@waxdigital.com>
2018-02-08 15:46:01 +0000

    rename dialog and minor responses added

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -5,6 +7,8 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Web.Http.Description;
 using System.Net.Http;
+using Autofac;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
@@ -19,45 +23,99 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         [ResponseType(typeof(void))]
         public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
-            // check if activity is of type message
-            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            if (activity != null)
             {
-                await Conversation.SendAsync(activity, () => new EchoDialog());
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        //await Conversation.SendAsync(activity, () => new EchoDialog());
+                        //await Conversation.SendAsync(activity, () => EchoCommandDialog.dialog);
+                        //await Conversation.SendAsync(activity, () => new EchoAttachmentDialog());
+                        await Conversation.SendAsync(activity, MakeRoot);
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                        IConversationUpdateActivity update = activity;
+                        using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, activity))
+                        {
+                            var client = scope.Resolve<IConnectorClient>();
+                            if (update.MembersAdded.Any())
+                            {
+                                var reply = activity.CreateReply();
+                                foreach (var newMember in update.MembersAdded)
+                                {
+                                    if (newMember.Id != activity.Recipient.Id)
+                                    {
+                                        reply.Text = $"Welcome to the Wax Bot, {newMember.Name}!";
+                                    }
+                                    //else
+                                    //{
+                                    //    reply.Text = $"Welcome to the Wax Bot, {activity.From.Name}";
+                                    //}
+
+                                    await client.Conversations.ReplyToActivityAsync(reply);
+                                }
+                            }
+                        }
+
+                        break;
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    case ActivityTypes.Ping:
+                    default:
+                        Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
+                }
             }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
+
             return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+
+
+            // check if activity is of type message
+            //if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            //{
+            //    await Conversation.SendAsync(activity, () => new EchoDialog());
+            //}
+            //else
+            //{
+            //    HandleSystemMessage(activity);
+            //}
+
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        private static IDialog<object> MakeRoot()
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
+            return Chain.From(() => new RootDialog());
         }
+
+        //private Activity HandleSystemMessage(Activity message)
+        //{
+        //    if (message.Type == ActivityTypes.DeleteUserData)
+        //    {
+        //        // Implement user deletion here
+        //        // If we handle user deletion, return a real message
+        //    }
+        //    else if (message.Type == ActivityTypes.ConversationUpdate)
+        //    {
+        //        // Handle conversation state changes, like members being added and removed
+        //        // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+        //        // Not available in all channels
+        //    }
+        //    else if (message.Type == ActivityTypes.ContactRelationUpdate)
+        //    {
+        //        // Handle add/remove from contact lists
+        //        // Activity.From + Activity.Action represent what happened
+        //    }
+        //    else if (message.Type == ActivityTypes.Typing)
+        //    {
+        //        // Handle knowing tha the user is typing
+        //    }
+        //    else if (message.Type == ActivityTypes.Ping)
+        //    {
+        //    }
+
+        //    return null;
+        //}
     }
 }
--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -1,15 +1,15 @@
 using System;
 using System.Threading.Tasks;
-
+using System.Web.ModelBinding;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
-using System.Net.Http;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
     [Serializable]
-    public class EchoDialog : IDialog<object>
+    public class RootDialog : IDialog<object>
     {
         protected int count = 1;
 
@@ -22,20 +22,46 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         {
             var message = await argument;
 
+            if (message.Text == "hi" || message.Text == "hello")
+            {
+                await context.PostAsync("Hello to you too!");
+                return;
+            }
+
             if (message.Text == "reset")
             {
                 PromptDialog.Confirm(
                     context,
                     AfterResetAsync,
                     "Are you sure you want to reset the count?",
-                    "Didn't get that!",
-                    promptStyle: PromptStyle.Auto);
+                    "Didn't get that!");
+
+                return;
+
             }
-            else
+
+            if (message.Text == "bye")
             {
-                await context.PostAsync($"{this.count++}: You said: '{message.Text}'");
-                context.Wait(MessageReceivedAsync);
+                
+                return;
             }
+
+
+            if (message.Text == "status")
+            {
+                await context.PostAsync("Status is Good!!!");
+                return;
+            }
+
+            if (message.Text == "weather")
+            {
+                await context.PostAsync("It's raining. Just a guess!");
+                return;
+            }
+
+            await context.PostAsync($"{this.count++}: You said: '{message.Text}'");
+            context.Wait(MessageReceivedAsync);
+         
         }
 
         public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
